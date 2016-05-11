0dd52e6dc8d2f18b1ec477d690ba0248995be837
Filchakov Denys <filchakov.denis@gmail.com>
2016-05-11 18:30:35 +0300

    fix link to server (REST API)

--- bootstrap.php
+++ bootstrap.php
@@ -7,6 +7,6 @@ ini_set("soap.wsdl_cache_enabled", "0");
 error_reporting(E_ALL);
 
 define("WEBSERVICE_URL", "http://soap_service.local/v1/");
-define("REST_SERVER", "https://services.zenduit.com:3041/api/v1/");
+define("REST_SERVER", "https://zenworkapi.zenduit.com:3041/api/v1/");
 
 $app = new Slim\App();
