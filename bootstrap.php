c2861b2f21568b73acbaaf38b073cd1b407417c9
Filchakov Denys <filchakov.denis@gmail.com>
2016-05-11 19:58:08 +0300

    move root path for web server

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,14 @@
 <?php
 
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 
 ini_set('display_errors', 'On');
 ini_set("soap.wsdl_cache_enabled", "0");
 error_reporting(E_ALL);
 
-define("WEBSERVICE_URL", "http://soap_service.local/v1/");
+define("CURRENT_VERSION", "v1");
+define("WEBSERVICE_URL", "http://" . $_SERVER['HTTP_HOST'] . "/". CURRENT_VERSION ."/");
+
 define("REST_SERVER", "https://zenworkapi.zenduit.com:3041/api/v1/");
 
 $app = new Slim\App();
--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
-include_once('../bootstrap.php');
+include_once('bootstrap.php');
 
 use Zend\Soap\Server as SoapServer;
 
 
 //Use slim group route to defined web service version
 $app->group('/v1', function() use ($app){
-    include_once('../Modules/Contact/routes/route.php');
-    include_once('../Modules/Location/routes/route.php');
+    include_once('Modules/Contact/routes/route.php');
+    include_once('Modules/Location/routes/route.php');
 });
 
 $app->run();
